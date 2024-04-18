baab496876275cd08ce50e8bfd971eae278c85d8
Felix Biwott <felixkpt@gmail.com>
2024-04-18 11:12:55 +0300

    added ability to create config/nestedroutes.php for basic config

--- src/Providers/NestedRoutesBindingServiceProvider.php
+++ src/Providers/NestedRoutesBindingServiceProvider.php
@@ -14,17 +14,42 @@ class NestedRoutesBindingServiceProvider extends ServiceProvider
     public function register()
     {
 
-        // Get the folder name after 'nested-routes' from environment configuration
-        $admin_routes_folder = preg_replace('#/+#', '/', env('ADMIN_NESTED_ROUTES_FOLDER', 'nested-routes/admin'));
-        $client_routes_folder = preg_replace('#/+#', '/', env('CLIENT_NESTED_ROUTES_FOLDER', 'nested-routes/client'));
-
-        // Set the 'nested_routes.admin_folder' configuration key to the value of 'ADMIN_NESTED_ROUTES_FOLDER/CLIENT_NESTED_ROUTES_FOLDER'
-        config([
-            'nested_routes.admin_folder' => $admin_routes_folder,
-            'nested_routes.client_folder' => $client_routes_folder,
-        ]);
+        $this->configure();
 
         $this->app->register(NestedRoutesMacroServiceProvider::class);
         $this->app->register(NestedRoutesServiceProvider::class);
+    }
+
+    public function configure()
+    {
+        $configPath = config_path('nestedroutes.php');
+
+        // Check if the config file already exists
+        if (!file_exists($configPath)) {
+            // If not, create it with default configuration
+            $this->createDefaultConfig($configPath);
+        }
+    }
+
+    private function createDefaultConfig($configPath)
+    {
+        $defaultConfig = [
+            'folder' => 'nested-routes',
+            'permissions' => [
+                'ignored_folders' => env('permissions_ignored_folders', [
+                    'auth',
+                    'client',
+                ]),
+            ],
+            'rename_main_folders' => [
+                'admin' => 'dashboard'
+            ]
+        ];
+
+        // Convert array to PHP code
+        $configContent = "<?php\n\nreturn " . var_export($defaultConfig, true) . ";\n";
+
+        // Write config content to file
+        file_put_contents($configPath, $configContent);
     }
 }
