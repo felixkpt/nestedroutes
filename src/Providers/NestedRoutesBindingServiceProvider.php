77ae9027097a46815561a226439938b4d43e8f3c
Felix Biwott <felixkpt@gmail.com>
2024-04-18 12:06:55 +0300

    middleware

--- src/Providers/NestedRoutesBindingServiceProvider.php
+++ src/Providers/NestedRoutesBindingServiceProvider.php
@@ -20,8 +20,23 @@ class NestedRoutesBindingServiceProvider extends ServiceProvider
         $this->app->register(NestedRoutesServiceProvider::class);
     }
 
-    function boot() {
+    function boot()
+    {
+        $this->registerMiddleware();
         $this->configure();
+    }
+
+    /**
+     * Register the middleware.
+     *
+     * @return void
+     */
+    protected function registerMiddleware()
+    {
+        // Register middleware using the 'middleware' method
+        $this->app['router']->middleware([
+            'nestedroutes.auth' => \Felixkpt\Nestedroutes\Http\Middleware\NestedRoutesAuthMiddleware::class,
+        ]);
     }
 
     public function configure()
