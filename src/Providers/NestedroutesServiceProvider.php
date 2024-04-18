e1b7203b0ca862ccc89863cc8226b5cf99d53f0e
Felix Biwott <felixkpt@gmail.com>
2024-04-18 20:51:06 +0300

    added texts

--- src/Providers/NestedroutesServiceProvider.php
+++ src/Providers/NestedroutesServiceProvider.php
@@ -5,6 +5,7 @@ namespace Felixkpt\Nestedroutes\Providers;
 use Felixkpt\Nestedroutes\Http\Middleware\NestedroutesAuthMiddleware;
 use Felixkpt\Nestedroutes\Http\Middleware\TemporaryTokenValidationMiddleware;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class NestedroutesServiceProvider extends ServiceProvider
@@ -30,7 +31,35 @@ class NestedroutesServiceProvider extends ServiceProvider
         $router->aliasMiddleware('nestedroutes.auth', NestedroutesAuthMiddleware::class);
         $router->aliasMiddleware('nestedroutes.temporary_token', TemporaryTokenValidationMiddleware::class);
 
-        $driver = preg_replace('@/+@', '/', 'routes/nested-routes/driver.php');
-        $this->loadRoutesFrom(base_path($driver));
+        $folder = base_path(preg_replace('@/+@', '/', 'routes/nested-routes/'));
+        File::ensureDirectoryExists($folder);
+
+        $driver = preg_replace('@/+@', '/', $folder . '/driver.php');
+
+        if (!file_exists($driver)) {
+            $contents = file_get_contents(__DIR__ . '/../../texts/driver.txt', 'r');
+            File::put($driver, $contents);
+            chmod($driver, 775);
+        }
+
+        $auth = preg_replace('@/+@', '/', $folder . '/auth.route.php');
+        if (!file_exists($auth)) {
+            $contents = file_get_contents(__DIR__ . '/../../texts/auth.route.txt', 'r');
+            File::put($auth, $contents);
+            chmod($auth, 775);
+        }
+
+
+        $folder = base_path(preg_replace('@/+@', '/', 'app/Http/Controllers/Auth'));
+        File::ensureDirectoryExists($folder);
+
+        $auth_controller = preg_replace('@/+@', '/', $folder . '/AuthController.php');
+        if (!file_exists($auth_controller)) {
+            $contents = file_get_contents(__DIR__ . '/../../texts/Auth/AuthController.txt', 'r');
+            File::put($auth_controller, $contents);
+            chmod($auth_controller, 775);
+        }
+
+        $this->loadRoutesFrom($driver);
     }
 }
