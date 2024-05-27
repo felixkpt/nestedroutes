7ed8023e7ee48b52f849f49df1517ce647348590
Felix Biwott <felixkpt@gmail.com>
2024-05-27 11:26:49 +0300

    updated nested routes

--- src/config/nestedroutes.php
+++ src/config/nestedroutes.php
@@ -4,30 +4,27 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Nested Routes config
+    | Nested Routes Configuration
     |--------------------------------------------------------------------------
     |
-    | This option controls the nested routes behavior.
+    | This option controls the behavior of nested routes.
     |
     */
-    'folder' => 'nested-routes',
-    'prefix' => 'api',
-    'middleWares' => ['api', 'nesteroutes.auth'],
+
+    'folder' => 'nested-routes', // Folder where nested routes are stored
+    'prefix' => 'api', // Prefix for nested routes
+    'middlewares' => ['api', 'nesteroutes.auth'], // Middlewares applied to nested routes
     'permissions' => [
         'ignored_folders' => env('permissions_ignored_folders', [
-            'auth',
+            'auth', // Ignored folders for permissions
             'client',
         ]),
     ],
-
-    'rename_main_folders' => [
-        'admin' => 'dashboard',
+    'rename_root_folders' => [
+        'admin' => 'dashboard' // Rename root folders for nested routes
     ],
-    'guestRoleId' => 2,
-    'defaultPublicRoutes' => [
-        'dashboard/settings/role-permissions/roles/get-user-roles-and-direct-permissions',
-        'dashboard/settings/role-permissions/roles/view/{id}/get-role-menu',
-        'dashboard/settings/role-permissions/roles/view/{id}/get-role-route-permissions',
-        'file-repo/*',
-    ]
+    'expanded_root_folders' => [], // Expanded root folders for nested routes
+    // If the key is not provided, it means the website doesn't support guest mode,
+    // all unauthenticated requests will be redirected to login
+    'guestRoleId' => 1101,
 ];
